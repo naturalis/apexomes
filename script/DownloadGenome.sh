3b7be83d2d34d8207d5b5f5024319969cb1bfe79
sdevriend <sebastiaandevriend@gmail.com>
2015-10-29 17:40:18 +0100

    Removed bugs with checking chromosome 21 and 22. Also removed the specific species, so this can also download homo-sapien files for example.

--- script/DownloadGenome.sh
+++ script/DownloadGenome.sh
@@ -2,26 +2,23 @@
 # It is easier to maintain a different script for the reference genome, because this uses a different checksum command, and
 # gets the files feeded from the checksum file prodivded by ensembl.
 # The script is not tested with BWA. There is room to update for the files to be BWA ready.
+# Not species specific.
 function WithoutCheckSums(){
-	filelist="$( cat weblist | egrep "Gorilla_gorilla.gorGor(.*).dna.chromosome.[^MT]" )"
-	for file in ${filelist[@]}
+	filelist="$(  echo ${weblist} | tr ' ' '\n' | egrep  ".*\.(.*).dna.chromosome.(^MT|((1|2)|[0-9])|(X|Y))"  | tr "\n" " " )"
+	while read  line
 	do
-		wget ${file}
-		gunzip ${file}
-	done
+		wget ${line}
+		
+	done < <(echo "$filelist")
+	gunzip *
 }
 
 function WithCheckSums(){
 	# Downloading checksums
 	`wget ${BaseUrl}"CHECKSUMS"`
-	
-	# File for listing failed downloads. This file can be checked in the complete pipeline for missing files. 
 	# I advice doing this with a simple egrep and exit if there is any file in the failedChromosomes.
-	touch failedChromosomes
-
-	`cat checksums | egrep "Gorilla_gorilla.gorGor(.*).dna.chromosome.[^MT]" > fastalist`
-
-	while read line
+	fastalist="$(cat checksums | egrep  ".*\.(.*).dna.chromosome.(^MT|((1|2)|[0-9])|(X|Y))" | awk '{ print $0} ') "
+	while read  line
 	do
 		name="$(echo $line | awk '{print $3}')"
 		# if the file does not exists, then do code.
@@ -30,37 +27,46 @@ function WithCheckSums(){
 			#Download file.
 			`wget ${BaseUrl}${name}`
 			# create a checksum for the downloaded file.
-			check="$(sum ${name})"
+			check="$(sum ${name} | awk '{print $1, $2}') "
 			# get colums with checksums from line.
 			checkfromfile="$(echo ${line} | awk '{print $1, $2}')"
-			if [ "$check" = "$checkfromfile" ]
+			
+			check2="$(echo "${check}" | egrep "${checkfromfile}")"
+			if [ "${check}" == "${checkfromfile}" ] || [ "${#check2}" -gt 0 ]
 			then
 				# if the checksum matches, then extract file from gz.
 				`gunzip ${name}`
+			
 			else
+				# File for listing failed downloads. This file can be checked in the complete pipeline for missing files. 
 				# if file doesn't matches, extend the failedChromosomes list.
 				echo ${name} >> failedChromosomes
 				rm ${name}
 			fi
 		fi
-	done < fastalist
+	done < <(echo "$fastalist")
 }
 
 
 #Script downloads the genome from the Gorilla. Location can be provided in the variable below.
-BaseUrl="ftp://ftp.ensembl.org/pub/release-82/fasta/gorilla_gorilla/dna/" # with checksums
-#BaseUrl="ftp://ftp.ensembl.org/pub/release-58/fasta/gorilla_gorilla/dna/"
+#BaseUrl="ftp://ftp.ensembl.org/pub/release-82/fasta/gorilla_gorilla/dna/" # with checksums
+BaseUrl="ftp://ftp.ensembl.org/pub/release-58/fasta/gorilla_gorilla/dna/"
+#BaseUrl="ftp://ftp.ensembl.org/pub/release-82/fasta/homo_sapiens/dna/"
+
 # FILL ME IN FIRST!
 # Could be prodivded by main pipeline.
-version="$(echo $BaseUrl | awk -F '/' '{print $5}')"
 Path="/media/sf_D_DRIVE/ape/dl_gen/"
-DIRNAME=${Path}Refgenome_${version}
+version="$(echo $BaseUrl | awk -F '/' '{print $5}')"
+Species="$(echo $BaseUrl | awk -F '/' '{print $7}')"
+DIRNAME=${Path}Refgenome_${version}_${Species}
 `mkdir ${DIRNAME}`
 # for saving the files and using local paths.
 cd ${DIRNAME}
 wget ${BaseUrl}
-`cat index.html | egrep "a href" | awk -F "<" '{ print $2 }' |  awk -F '"' '{ print $2 }' > weblist `
-if [ `cat weblist | egrep "CHECKSUMS" ` ]
+weblist="$(cat index.html | egrep "a href" | awk -F "<" '{ print $2 }' |  awk -F '"' '{ print $2 }' )"
+webcheck="$(echo ${weblist} | egrep "CHECKSUMS" )"
+rm index.html
+if [ "${webcheck}"  ]
 then
 	# if checksums available, then do downloading with checksums.
 	WithCheckSums
