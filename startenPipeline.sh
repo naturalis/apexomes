b85472a4ca08de1047c92ed8321383fd8e853f1d
Ubuntu <sebastiaandevriend@gmail.com>
2016-01-27 15:00:34 +0100

    Bug in startenPipeline. Is easier to fix on windows...

--- startenPipeline.sh
+++ startenPipeline.sh
@@ -2,8 +2,37 @@
 
 # work in progres variables.
 # must be changed for the actual pipeline.
-path_output=/mnt/data/NewRefPipeline
+#path_output=/mnt/data/NewRefPipeline
+RefGenome="$(cat config.txt | egrep -v '#' | head -n 1)"
+echo "The following settings where found:"
+echo ${RefGenome}
+yesno="y n"
+echo "Is this correct?"
+select ant in ${yesno}
+    do
+	if [ "${REPLY,,}" = 'y' ]
+	then
+	    break
+        else if [ "${REPLY,,} = 'n'
+        then
+            echo "Please check your config.txt file and then run the pipeline again"
+            exit
+	fi	
+    done
 
+echo "Do you want to trim the reads?"
+#yesno="y n"
+select ans in ${yesno}
+    do
+	if [ "${REPLY,,}" = 'y' ] || [ "${REPLY,,}" = 'n' ]
+	
+	then
+	    Trim="${REPLY,,}"
+	    break
+        else
+            echo "Please select yes or no"
+	fi	
+    done
 echo "Start script: $(date +%T)"
 #read -p "Enter name for outputdirectory: " usrRunname
 usrRunname=SandraNew
@@ -24,9 +53,10 @@ outputDir=${path_output}/${usrRunname}
 inFile1=/mnt/data/Blij_Sandra/Generade_Gorilla-gorilla-gorilla_F_Sandra_EAZA-Studbook-9_L007_R1_001.fastq.gz
 inFile2=/mnt/data/Blij_Sandra/Generade_Gorilla-gorilla-gorilla_F_Sandra_EAZA-Studbook-9_L007_R2_001.fastq.gz
 
-
+# debug
+echo ${Trim}
 
 echo "Start pipeline: $(date +%T)"
 # hier eerste script voor pipeline aanroepen.
 # denk aan het meegeven van de outputdir!
-bash apexomesPipeline.sh ${outputDir} ${inFile1} ${inFile2} ${path_output} ${usrRunname}
+#bash apexomesPipeline.sh ${outputDir} ${inFile1} ${inFile2} ${path_output} ${usrRunname}
