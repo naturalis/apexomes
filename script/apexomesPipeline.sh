3bd55805e2674567981d13e4e8bef74bc97dc40c
Ubuntu <sebastiaandevriend@gmail.com>
2016-02-03 11:48:57 +0100

    scripts naar github

--- script/apexomesPipeline.sh
+++ script/apexomesPipeline.sh
@@ -1,12 +1,38 @@
 #!/bin/bash
+Trim=$1
 # Declaration of variables.
-RefGenome="$(cat config.txt | egrep -v '#' | head -n 1)"
-ResultDir="$(cat config.txt | egrep -v '#' | head -n 2 | tail -n 1)"
-arDataDir="$(cat config.txt | egrep -v '#' | awk '{if(NR>2)print}')"
+RefGenome="$(cat ../config.txt | egrep -v '#' | head -n 1)"
+ResultDir="$(cat ../config.txt | egrep -v '#' | head -n 2 | tail -n 1)"
+arDataDir="$(cat ../config.txt | egrep -v '#' | awk '{if(NR>2)print}')"
 for monkey in ${arDataDir}
 do
-    echo ${monkey}
+    # For each sample in the Gorilla data
+    # get sample name with basename
+    name="$( basename ${monkey} )"
+    # Convert samplename to path
+    samplepath=${ResultDir}/${name}
+    # Create sub directory in result directory if
+    # directory doesn't excist.
+    if [ ! -d "${samplepath}" ]
+    then
+        mkdir ${samplepath}
+    fi
+    # If user specified trimming. The trimscript will
+    # be called from here.
+    if [ "${Trim}" = 'y' ]
+    then
+        # Please specify trim command.
+    else
+        # Move reads to resultfolder so mapping script
+        # can grab the files and place result files on
+        # right location.
+        # PLEASE CHECK COMMAND
+        #"$( cp ${monkey}/*.gz ${samplepath}/ )"
+    fi
+    #./
+    
 done
+
 #outputDir=$1
 #inFile1=$2
 #inFile2=$3
