16582e518886680a1324586a843840b8ca6cc572
Rutger Vos <rutgeraldo@gmail.com>
2016-06-22 13:59:10 -0500

    Create env.sh

---
+++ conf/env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This file sets up the environment variables for the analysis. This so that we 
+# don't have to fiddle with command line arguments or hack into the config.txt.
+
+# The location of the data volume
+export DATA=/mnt/data
+
+# Where the input data are located
+export INPUT=$DATA/input
+
+# The location of the reference genome, as unzipped FASTA
+export REFERENCE=$INPUT/fasta/Gorilla_gorilla.gorGor3.1.71.dna.toplevel.fa
