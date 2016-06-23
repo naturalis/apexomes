7eb64f15aabc116fbe43722e54a9947b99f272f3
Rutger Vos <rutgeraldo@gmail.com>
2016-06-22 21:31:14 -0500

    Update variantcalling.sh

--- script/variantcalling.sh
+++ script/variantcalling.sh
@@ -3,10 +3,16 @@
 # variantcalling.sh ReferenceGenome InputDirectory
 # for example
 # ./variantcalling.sh /mnt/data/refgenome /mnt/data/pipeline/monkey1
-RefGenome=$1
+REFERENCE=$1
 DataDir=$2
 name="$( basename ${DataDir} )"
-freebayes --fasta-reference ${RefGenome} ${DataDir}/*.sam.sorted.bam --standard-filters --no-population-priors -p 2 --report-genotype-likelihood-max --prob-contamination 0.05 > ${DataDir}/${name}.vcf
+
+# Call the SNPs using freebayes
+freebayes --fasta-reference ${REFERENCE} ${DataDir}/*.sam.sorted.bam \
+  --standard-filters --no-population-priors -p 2 --report-genotype-likelihood-max \
+  --prob-contamination 0.05 > ${DataDir}/${name}.vcf
+
+# Insert the sample name in the VCF
 sed "s/unknown/${name}/" ${DataDir}/${name}.vcf > ${DataDir}/temp.vcf
 rm ${DataDir}/${name}.vcf
 mv ${DataDir}/temp.vcf ${DataDir}/${name}.vcf
